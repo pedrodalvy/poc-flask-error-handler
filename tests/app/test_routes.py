5fdd8082014b684fa7527ee6de96676006d75a56
Pedro Dalvy <pedrodalvy@outlook.com>
2024-12-07 17:48:44 -0300

    test: implement tests to error handlers

--- tests/app/test_routes.py
+++ tests/app/test_routes.py
@@ -11,8 +11,27 @@ def client() -> FlaskClient:
     with app.test_client() as client:
         yield client
 
+
 def test_index(client: FlaskClient):
     response = client.get('/ping')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json == {'message': 'pong'}
+
+
+def test_not_found(client: FlaskClient):
+    params = {'action': 'not_found'}
+
+    response = client.get('/ping', query_string=params)
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json == {'message': 'Not Found'}
+
+
+def test_not_handled(client: FlaskClient):
+    params = {'action': 'not_handled'}
+
+    response = client.get('/ping', query_string=params)
+
+    assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
+    assert response.json == {'message': 'Internal Server Error'}
